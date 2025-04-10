1c5b3449d2d1bf855844e701e11edc8c11757335
Petr Chatrný <posta@petrchatrny.cz>
2025-04-10 21:43:18 +0200

    Implement rgb_camera.py via pypylon

--- backend/src/common/enum.py
+++ backend/src/common/enum.py
@@ -2,8 +2,8 @@ from enum import StrEnum
 
 
 class ImageFormat(StrEnum):
-    BMP = "BMP"
-    JPEG = "JPEG"
+    BMP = "BMP"  # TODO unsupported by RGB Camera
+    JPEG = "JPEG"  # TODO unsupported by RGB Camera
     TIFF = "TIFF"
     PNG = "PNG"
     RAW = "RAW"
