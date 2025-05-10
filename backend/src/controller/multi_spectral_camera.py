944204d6bed252ed051484d4f847978ca4ef2e94
Petr Chatrný <posta@petrchatrny.cz>
2025-05-10 17:03:02 +0200

    Add controlling of MS camera

--- backend/src/controller/multi_spectral_camera.py
+++ backend/src/controller/multi_spectral_camera.py
@@ -1,4 +1,6 @@
+import subprocess
 from datetime import datetime
+from subprocess import Popen
 
 from common.enum import ImageFormat
 
@@ -7,12 +9,23 @@ class MultiSpectralCameraController:
 
     def __init__(self, output_dir: str):
         self.output_dir = output_dir
+        self._process: Popen | None = None
 
-    def start_capturing(self, img_format: ImageFormat):
-        pass
+    def start_capturing(self, img_format: ImageFormat, width: int, height: int, interval: int, exposure_time: int):
+        if self._process is None:
+            # TODO použít spíš přímo LabViewController?
+            self._process = subprocess.Popen([
+                "python", "../../../labview/labview-control/camera-control.py",
+                "--image_path", f"{self.output_dir}",
+                "--interval", f"{interval}",
+                "--exposure_time", f"{exposure_time}",
+                "--width", f"{width}",
+                "--height", f"{height}"
+            ])
 
     def stop_capturing(self):
-        pass
+        if self._process is not None:
+            self._process.kill()
 
     @staticmethod
     def generate_name(img_format: ImageFormat) -> str:
